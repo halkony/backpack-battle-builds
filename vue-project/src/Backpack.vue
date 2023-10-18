83c88b0827b114593d8d8f8fbef6e19ebc4ed04f
halkony <nathan.halko@gmail.com>
2023-10-18 17:05:58 -0500

    Basic App complete, can add items and bags and shit

--- vue-project/src/Backpack.vue
+++ vue-project/src/Backpack.vue
@@ -1,112 +1,81 @@
 <script setup>
 import * as PIXI from "pixi.js";
 import { onMounted } from "vue";
-import AceOfSpades from "./AceOfSpades.png";
-import Banana from "../img/items/Banana.png"
-import itemsJSON from './items.json'
+import notbagsJSON from "./notbags.json";
+import bagsJSON from "./bags.json";
 
 // make sure the bags are on a lower layer than the items
 
+const app = new PIXI.Application({
+  background: "#1099bb",
+  width: 720,
+  height: 600,
+});
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xffff00);
+
+const gridWidth = 9;
+const gridHeight = 7;
+
+const SLOT_SIZE = 70;
+
+// draw rectangles in a grid
+for (let i = 0; i < gridWidth; i++) {
+  for (let j = 0; j < gridHeight; j++) {
+    if ((i + j) % 2 == 0) {
+      graphics.beginFill(0x777777);
+    } else {
+      graphics.beginFill(0x444444);
+    }
+    graphics.drawRect(i * SLOT_SIZE, j * SLOT_SIZE, SLOT_SIZE, SLOT_SIZE);
+  }
+}
+
+let dragTarget = null;
+
+app.stage.eventMode = "static";
+app.stage.hitArea = app.screen;
+app.stage.on("pointerup", onDragEnd);
+app.stage.on("pointerupoutside", onDragEnd);
+
+
+function createItem(itemName, x, y) {
+  // create our little bunny friend..
+  const texture = PIXI.Texture.from(`http://backpack-battles.s3.amazonaws.com/items/${itemName}.png`);
+  const item = new PIXI.Sprite(texture);
+
+  // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+  item.eventMode = "static";
+
+  // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+  item.cursor = "pointer";
+
+  // center the bunny's anchor point
+  item.anchor.set(0.5);
+  // set item.pivot to the sprites center
+  // item.pivot.set(item.width / 2, item.height / 2);
+
+  // make it a bit bigger, so it's easier to grab
+  item.scale.set(SLOT_SIZE * .006);
+
+  // setup events for mouse + touch using
+  // the pointer events
+  item.on("pointerdown", onDragStart, item);
+
+  // move the sprite to its designated position
+  item.x = x;
+  item.y = y;
+
+  // add it to the stage
+  app.stage.addChild(item);
+}
+
+let addItemToBackpack = (itemName) => {
+    console.log(`Adding ${itemName} to backpack...`);
+    createItem(itemName, 500, 500)
+}
+
 onMounted(() => {
-  const app = new PIXI.Application({
-    background: "#1099bb",
-    width: 640,
-    height: 360,
-  });
-  let sprite = PIXI.Sprite.from(AceOfSpades);
-  app.stage.addChild(sprite);
-  var graphics = new PIXI.Graphics();
-  graphics.beginFill(0xffff00);
-
-  const gridWidth = 9;
-  const gridHeight = 7;
-
-  // draw rectangles in a grid
-  for (let i = 0; i < gridWidth; i++) {
-    for (let j = 0; j < gridHeight; j++) {
-      if ((i + j) % 2 == 0) {
-        graphics.beginFill(0x777777);
-      } else {
-        graphics.beginFill(0x444444);
-      }
-      graphics.drawRect(i * 50, j * 50, 50, 50);
-    }
-  }
-
-  let dragTarget = null;
-
-  app.stage.eventMode = "static";
-  app.stage.hitArea = app.screen;
-  app.stage.on("pointerup", onDragEnd);
-  app.stage.on("pointerupoutside", onDragEnd);
-
-  function createItem(itemImage, x, y) {
-    // create our little bunny friend..
-    const texture = PIXI.Texture.from(itemImage);
-    const item = new PIXI.Sprite(texture);
-
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    item.eventMode = "static";
-
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    item.cursor = "pointer";
-
-    // center the bunny's anchor point
-    item.anchor.set(0.5);
-    // set item.pivot to the sprites center
-    // item.pivot.set(item.width / 2, item.height / 2);
-
-    // make it a bit bigger, so it's easier to grab
-    item.scale.set(0.5);
-
-    // setup events for mouse + touch using
-    // the pointer events
-    item.on("pointerdown", onDragStart, item);
-
-    // move the sprite to its designated position
-    item.x = x;
-    item.y = y;
-
-    // add it to the stage
-    app.stage.addChild(item);
-  }
-  // if the item is being dragged, whenever r is press, the item rotates 90 degrees clockwise
-
-  function onDragMove(event) {
-    if (dragTarget) {
-      dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-    }
-  }
-
-  function onDragStart() {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    // this.data = event.data;
-    this.alpha = 0.5;
-    dragTarget = this;
-    app.stage.on("pointermove", onDragMove);
-  }
-
-  function onDragEnd() {
-    if (dragTarget) {
-      app.stage.off("pointermove", onDragMove);
-      dragTarget.alpha = 1;
-      dragTarget = null;
-    }
-  }
-
-  app.stage.addChild(graphics);
-  createItem(AceOfSpades, 100, 100);
-  createItem(Banana, 200, 200)
-
-  let elapsed = 0.0;
-  app.ticker.add((delta) => {
-    elapsed += delta;
-    sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-    sprite.rotation += 0.05;
-  });
-  //   let container = document.body.getElementById("canvas");
   document.body.appendChild(app.view);
 
   const ROTATIONAMOUNT = (2 * Math.PI) / 4;
@@ -120,6 +89,40 @@ onMounted(() => {
     }
   });
 });
+// if the item is being dragged, whenever r is press, the item rotates 90 degrees clockwise
+
+function onDragMove(event) {
+  if (dragTarget) {
+    dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+  }
+}
+
+function onDragStart() {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  // this.data = event.data;
+  this.alpha = 0.5;
+  dragTarget = this;
+  app.stage.on("pointermove", onDragMove);
+}
+
+function onDragEnd() {
+  if (dragTarget) {
+    app.stage.off("pointermove", onDragMove);
+    dragTarget.alpha = 1;
+    dragTarget = null;
+  }
+}
+
+createItem("AceofSpades", 100, 100)
+app.stage.addChild(graphics);
+
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+});
+//   let container = document.body.getElementById("canvas");
 </script>
 
 <template>
@@ -129,10 +132,17 @@ onMounted(() => {
                 Create a 1x2 item.
                 Place and rotate the item on the grid
     -->
-    <select name="cars" id="cars">
-        <option v-for="item in itemsJSON" :value="item">{{item}}</option>
-    </select>
+  <select name="selectedItem" id="selectedItem" v-model="selectedItem">
+    <option v-for="notbag in notbagsJSON" :value="notbag">{{ notbag }}</option>
+  </select>
+  <button @click="addItemToBackpack(selectedItem)">Add to Backpack</button>
+
+  <select name="selectedBag" id="selectedBag" v-model="selectedBag">
+    <option v-for="bag in bagsJSON" :value="bag">{{ bag }}</option>
+  </select>
+  <button @click="addItemToBackpack(selectedBag)">Add to Backpack</button>
 </template>
 
 <style>
+
 </style>
