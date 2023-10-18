9254a088e7ea50cec59139d15eeb705cec20ece6
halkony <nathan.halko@gmail.com>
2023-10-18 15:32:03 -0500

    Added basic drag and drop stuff

--- vue-project/src/Backpack.vue
+++ vue-project/src/Backpack.vue
@@ -28,10 +28,69 @@ onMounted(() => {
       graphics.drawRect(i * 50, j * 50, 50, 50);
     }
   }
-  // set the line style to have a width of 5 and set the color to red
-  graphics.lineStyle(10, 0xff0000);
+
+  let dragTarget = null;
+
+app.stage.eventMode = 'static';
+app.stage.hitArea = app.screen;
+app.stage.on('pointerup', onDragEnd);
+app.stage.on('pointerupoutside', onDragEnd);
+
+  const texture = PIXI.Texture.from("https://pixijs.com/assets/bunny.png");
+  function createItem(x, y) {
+    // create our little bunny friend..
+    const bunny = new PIXI.Sprite(texture);
+
+    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+    bunny.eventMode = "static";
+
+    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+    bunny.cursor = "pointer";
+
+    // center the bunny's anchor point
+    bunny.anchor.set(0.5);
+
+    // make it a bit bigger, so it's easier to grab
+    bunny.scale.set(3);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    bunny.on("pointerdown", onDragStart, bunny);
+
+    // move the sprite to its designated position
+    bunny.x = x;
+    bunny.y = y;
+
+    // add it to the stage
+    app.stage.addChild(bunny);
+  }
+
+  function onDragMove(event) {
+    if (dragTarget) {
+      dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+    }
+  }
+
+  function onDragStart() {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    // this.data = event.data;
+    this.alpha = 0.5;
+    dragTarget = this;
+    app.stage.on("pointermove", onDragMove);
+  }
+
+  function onDragEnd() {
+    if (dragTarget) {
+      app.stage.off("pointermove", onDragMove);
+      dragTarget.alpha = 1;
+      dragTarget = null;
+    }
+  }
 
   app.stage.addChild(graphics);
+  createItem(100, 100);
 
   let elapsed = 0.0;
   app.ticker.add((delta) => {
